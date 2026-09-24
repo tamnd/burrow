b57156e50e1f906fb382c0835316928f0ab0b190
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2026-09-25 01:09:14 +0700

    Make the ascii85 test build under MSVC
    MSVC does not accept a const variable as an initializer inside another
    array, so the long test pair is now a macro that both uses expand.

--- tests/ascii85_test.c
+++ tests/ascii85_test.c
@@ -38,22 +38,27 @@ typedef struct TestPair {
     {{(const Byte *)(dec), (Int)sizeof(dec) - 1},                                      \
      {(const Byte *)(enc), (Int)sizeof(enc) - 1}}
 
-static const TestPair bigtest = TP(
-    "Man is distinguished, not only by his reason, but by this singular passion from "
-    "other animals, which is a lust of the mind, that by a perseverance of delight in "
-    "the continued and indefatigable generation of knowledge, exceeds the short "
-    "vehemence of any carnal pleasure.",
-    "9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,\n"
-    "O<DJ+*.@<*K0@<6L(Df-\\0Ec5e;DffZ(EZee.Bl.9pF\"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKY\n"
-    "i(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIa\n"
-    "l(DId<j@<?3r@:F%a+D58'ATD4$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G\n"
-    ">uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c\n");
+/* A macro and not only a variable, because MSVC will not take a const
+ * variable as an initializer inside another array. */
+#define BIGTEST                                                                        \
+    TP("Man is distinguished, not only by his reason, but by this singular "           \
+       "passion from other animals, which is a lust of the mind, that by a "           \
+       "perseverance of delight in the continued and indefatigable generation of "     \
+       "knowledge, exceeds the short vehemence of any carnal pleasure.",               \
+       "9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,\n"   \
+       "O<DJ+*.@<*K0@<6L(Df-\\0Ec5e;DffZ(EZee.Bl.9pF\"AGXBPCsi+DGm>@3BB/F*&OCAfu2/"    \
+       "AKY\n"                                                                         \
+       "i(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIa\n" \
+       "l(DId<j@<?3r@:F%a+D58'ATD4$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G\n" \
+       ">uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c\n")
+
+static const TestPair bigtest = BIGTEST;
 
 static const TestPair pairs[] = {
     /* Encode returns 0 when len(src) is 0 */
     TP("", ""),
     /* Wikipedia example */
-    bigtest,
+    BIGTEST,
     /* Special case when shortening !!!!! to z. */
     TP("\000\000\000\000", "z"),
 };
